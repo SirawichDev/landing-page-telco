008085daa7e3134b9686c1124687b9037e5b767d
SirawichDev <sirawit0676@gmail.com>
2022-08-16 03:21:09 +0700

    tc-000: footer provided

--- app/components/layouts/main-layout.tsx
+++ app/components/layouts/main-layout.tsx
@@ -1,16 +1,17 @@
 import type { FC } from "react";
 import React from "react";
 import Header from "app/components/layouts/header";
+import Footer from '~/components/layouts/footer';
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen transition-colors bg-gray-100 duration-100">
+    <div className="flex flex-col min-h-screen transition-colors duration-100">
       <Header className=" md:h-16 lg:h-50" />
       {children}
-      <footer>s</footer>
+      <Footer/>
     </div>
   );
 };
